0e643b0e91c888d103e07d5d29165aa47ed1037a
Hugo Thiago <103154103+Huglh@users.noreply.github.com>
2024-02-28 10:14:27 -0300

    Add files via upload

---
+++ Arvore/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arvore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o número de linhas da pirâmide: ");
+            int numLinhas = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numLinhas; i++)
+            {
+                for (int j = 0; j < numLinhas - i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k < 2 * i + 1; k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
